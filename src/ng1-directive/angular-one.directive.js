da3bb89b18075de8f0380929f90899de840fdd84
Seema Shariat <sshariat@750-MAC-AFVH8.local>
2016-08-15 11:49:31 -0400

    updates scope to first directive to inherit only

--- src/ng1-directive/angular-one.directive.js
+++ src/ng1-directive/angular-one.directive.js
@@ -1,6 +1,6 @@
 myApp.directive("angularOne", function () {
     return {
-        scope: "=",
+        scope: "@",
         template: '<h2>{{lesson[0].title}}</h2>',
         controller: function ($scope) {
 	        
